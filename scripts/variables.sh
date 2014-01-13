4a40f7ef4794825756141369bb583ecf14fb5705
Mr Snow <mr.snow@houseoflaudanum.com>
2014-01-13 21:26:37 +1100

    Cleaner variables.

--- scripts/variables.sh
+++ scripts/variables.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-VERBOSE="-v --debug"
+# VERBOSE="-v --debug"
+VERBOSE=""
 
 PROFILE=bigdata
 DEST=local
-SITENAME="On Journal ($DEST)"
-TLD=onjournal.org
+SITENAME="Big Data ($DEST)"
+TLD=bigdata.example.com
 DOMAIN=$DEST.$TLD
 SITESUBDIR=$DOMAIN
 
-# MAKE="https://raw.github.com/Laudanum/bigdata/develop/build-bigdata.make?token=694770__eyJzY29wZSI6IlJhd0Jsb2I6TGF1ZGFudW0vb24vZGV2ZWxvcC9idWlsZC1vbnByb2ZpbGUubWFrZSIsImV4cGlyZXMiOjEzODk3MzAxNTV9--dfbc024eda95eb00e36d0ce8187b2f8b06211e59"
+MAKE="https://raw.github.com/Laudanum/bigdata/master/build-bigdata.make"
 
-WWWUSER=vagrant
+WWWUSER=rossetti
 
 DBUSER=vagrant
 DBPASS=$DBUSER
 DBHOST=127.0.0.1
-DBNAME=onjournal_org_local
+DBNAME=bigdata_example_com_local
 
-ACCOUNTNAME=laudanum
+ACCOUNTNAME=admin
 ACCOUNTPASS=$ACCOUNTNAME
-ACCOUNTEMAIL=mr.snow@houseoflaudanum.com
+ACCOUNTEMAIL=admin@example.com
 
